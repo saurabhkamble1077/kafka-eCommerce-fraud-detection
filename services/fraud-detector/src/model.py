da7e3ba878293695c5a03c7213a2b2b877983990
saurabhkamble1077 <saurabhkamble1077@gmail.com>
2025-05-04 15:45:00 +0530

    train later

---
+++ services/fraud-detector/src/model.py
@@ -0,0 +1,6 @@
+# TODO: train isolation forest and load here
+# placeholder for now
+
+def score_transaction(features: dict) -> float:
+    """Returns fraud probability 0.0-1.0. Stub for now."""
+    return 0.0
